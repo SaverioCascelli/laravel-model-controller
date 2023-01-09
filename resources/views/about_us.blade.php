348556a8cb0232299b01e0faf688fa06763fb432
SaverioCascelli <saverio.cascelli@gmail.com>
2023-01-09 17:35:24 +0100

    init

---
+++ resources/views/about_us.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.main')
+@section('content')
+    <h3>about us</h3>
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,10 @@
+<div class="navbar">
+    <ul class="d-flex">
+        <li class="px-3">
+            <a href="{{ route('home') }}">home</a>
+        </li>
+        <li class="px-3">
+            <a href="{{ route('about_us') }}">about us</a>
+        </li>
+    </ul>
+</div>
